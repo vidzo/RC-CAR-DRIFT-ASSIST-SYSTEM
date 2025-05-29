15abb402f99b5e821650e7a3fa02e1a04c4042fb
vidzo <vidal.ayittey@gmail.com>
2025-05-29 23:04:33 +0100

    Update get_gyro.ino

--- drift_gyro/get_gyro.ino
+++ drift_gyro/get_gyro.ino
@@ -1,18 +1,6 @@
-
-
-inline void callimu()
-{
-  //-------EXTRACTION AND PROCESSING OF ACCEL-GYRO DATA BEGINS--
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); //calling imu for values
-  G[2]=gz;
-  G[2]-=offsetG[2]; // subtracting offset
-  G[2]*=0.030516;        //mapping to degrees per second
-  
-  //-----EXTRACTION AND PROCESSING OF ACCEL-GYRO DATA ENDS------
-  
+inline void callimu() {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  float rawGz = (gz - offsetGz) * 0.030516;
+  yawRateFiltered = gyroFilterAlpha * rawGz + (1 - gyroFilterAlpha) * yawRateFiltered;
+  G[2] = yawRateFiltered;
 }
-
-
-
-
-
